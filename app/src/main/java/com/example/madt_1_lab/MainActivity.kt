40c4a6a75a006bdd6c45191bc4c7410440171e3a
gireyosmanov <gireygame@gmail.com>
2023-09-10 15:05:35 +0300

    added button to change text

--- app/src/main/java/com/example/madt_1_lab/MainActivity.kt
+++ app/src/main/java/com/example/madt_1_lab/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.madt_1_lab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.madt_1_lab.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.changeText.setOnClickListener {
+            binding.textView.text = "rnd"
+        }
     }
 }
