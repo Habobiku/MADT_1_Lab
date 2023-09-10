6b1bb85235aefe02389d7e842377880ad2ca2170
gireyosmanov <gireygame@gmail.com>
2023-09-10 15:20:41 +0300

    added button to change color of text

--- app/src/main/java/com/example/madt_1_lab/MainActivity.kt
+++ app/src/main/java/com/example/madt_1_lab/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.madt_1_lab
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.madt_1_lab.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,8 +16,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        buttonClicker()
+    }
+
+    private fun buttonClicker() {
         binding.changeText.setOnClickListener {
             binding.textView.text = "rnd"
+        }
+        binding.changeColor.setOnClickListener {
+            binding.textView.setTextColor(Color.RED)
         }
     }
 }
